2685c6a4ded9efd7ce3a5394152f2be7ff296f7b
Dekokiyo <dekobokokiyomori0817@gmail.com>
2022-10-23 19:08:10 +0900

    CreateBase

---
+++ ClassicAmongUs/MeetingStart.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using HarmonyLib;
+
+namespace UltimateMods.ClassicAmongUs
+{
+    [HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
+    public static class ClassicAnimationPatch
+    {
+        public static bool WasCreated = false;
+        public static bool IsTimerStart = false;
+        public static int num;
+        public static float Timer;
+        public static void Prefix()
+        {
+            switch (AnimNum)
+            {
+                case 1:
+                    if (Timer <= 0f && ClassicAnimation != null)
+                    {
+                        SetOne();
+                    }
+                    Timer = Mathf.Max(0f, Timer -= Time.deltaTime);
+                    break;
+                case 2:
+                    if (Timer <= 0f && ClassicAnimation != null)
+                    {
+                        SetTwo();
+                    }
+                    Timer = Mathf.Max(0f, Timer -= Time.deltaTime);
+                    break;
+                case 3:
+                    if (Timer <= 0f && ClassicAnimation != null)
+                    {
+                        SetThree();
+                    }
+                    Timer = Mathf.Max(0f, Timer -= Time.deltaTime);
+                    break;
+                case 4:
+                    if (Timer <= 0f)
+                    {
+                        if (ClassicAnimation.transform.localScale.y > 0f && ClassicAnimation != null)
+                        {
+                            if (PushButton != null)
+                            {
+                                renderer2.sprite = null;
+                            }
+                            ClassicAnimation.transform.localScale -= new Vector3(0f, 0.25f, 0f);
+                        }
+                    }
+                    Timer = Mathf.Max(0f, Timer -= Time.deltaTime);
+                    break;
+            }
+        }
+    }
+}
